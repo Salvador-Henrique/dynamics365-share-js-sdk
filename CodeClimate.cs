98242ab6114d5738b617b39190eded73e7083458
Salvador-Henrique <84979740+Salvador-Henrique@users.noreply.github.com>
2021-06-15 22:07:07 -0300

    Teste do codeclimate

---
+++ CodeClimate.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CodeClimate
+{
+  public class CodeQuality
+  {
+     public CodeQuality() {};
+     
+     public void Execute(string variavel)
+     {
+        string x = variavel;
+        string nomecomerro= "erro";
+        
+     }
+  
+  }
+}
